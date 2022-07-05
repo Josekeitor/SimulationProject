ce527b1500eef98815de8c146c1f2744e83374ac
jc <jacosta291@gmail.com>
2022-07-04 20:43:03 -0500

    add folder to store input files

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import ChiSquaredTest as chi
 import numberGenerator as numGen
 
 print(numGen.randomNumbers(6, 32, 3, 80, 10))
-print("H0 is not rejected " if chi.chiSquaredPassed('chi_data-2.txt', 4) else "H0 is rejected")
+print("H0 is not rejected " if chi.chiSquaredPassed('InputFiles/chi_data-2.txt', 4) else "H0 is rejected")
