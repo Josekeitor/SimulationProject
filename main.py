3928b431123c0b60aab47fb3bb4eaf0736237255
David Martinez <34782922+dmtzt@users.noreply.github.com>
2022-07-06 20:02:12 -0500

    Integración de Runs Test en el script principal

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import chiSquaredTest as chi
 import numberGenerator as numGen
 import runsTest as runT
 
-print(numGen.randomNumbers(6, 32, 3, 80, 10))
-print("H0 is not rejected " if chi.chiSquaredPassed('InputFiles/chi_data-2.txt', 4) else "H0 is rejected")
+from pathlib import Path
 
-list = [10,20,5,2,7,15]   
-result_list = runT.checkSign(list)
-numberStreak = runT.getStreak(result_list)
-totalSign = runT.countSignNumber(result_list)
-expVal = runT.getExpectedValue(totalSign)
-variVal = runT.getVarinece(totalSign)
-runT.getRunTestResult(expVal,variVal,numberStreak)
+dir_input = Path('InputFiles')
+runs_data = Path(dir_input / 'runs_data.txt')
+chi_data = Path(dir_input / 'chi_data-2.txt')
+
+print(numGen.randomNumbers(6, 32, 3, 80, 10))
+print("H0 is not rejected " if chi.chiSquaredPassed(
+    chi_data, 4) else "H0 is rejected")
+
+print("H0 is not rejected " if runT.runsTestPassed(
+    runs_data, 4) else "H0 is rejected")
--- runsTest.py
+++ runsTest.py
@@ -1,3 +1,6 @@
+import math
+
+
 def checkSign(list):
     signList = []
     for i in range(len(list) - 1):
@@ -6,37 +9,58 @@ def checkSign(list):
         else:
             signList.append("-")
     print(signList)
-    return signList 
+    return signList
+
 
 def countSignNumber(signList):
     signNumber = len(signList)
     print(signNumber)
     return signNumber
-    
+
+
 def getStreak(signList):
     numberStreak = 0
     for i in range(len(signList)):
-        if i == 0 or signList[i] !=signList[i-1]:
+        if i == 0 or signList[i] != signList[i-1]:
             numberStreak = numberStreak + 1
     print(numberStreak)
     return numberStreak
+
 
 def getExpectedValue(numberStreak):
     formula = ((2*numberStreak)-1)/3
     return formula
 
+
 def getVarinece(totalSign):
-    resultFormula = ((16*totalSign)-29)/90
+    resultFormula = math.sqrt(((16*totalSign)-29)/90)
     return resultFormula
 
-def getRunTestResult(expVal,variVal, numberStreak):
-     
+
+def getRunTestResult(expVal, variVal, numberStreak):
     resultFormula = (numberStreak-expVal)/variVal
     tableValue = 1.96
-    print("H0 los numeros aparecen de manera aletoria, H1: los numeros no aparecen de forma aleatroia ")
+    print("H0 los numeros aparecen de manera aletoria, H1: los numeros no aparecen de forma aleatoria")
     print("Estadistico de prueba es:", resultFormula)
-    if  abs(resultFormula) < tableValue:
-        print("H0 no se rechaza")
-    else:
-        print("H1 se rechaza")
-    return resultFormula
+
+    return abs(resultFormula) < tableValue
+
+
+def readList(file_name, decimals):
+    file_data = open(file_name, 'r')
+    raw_data = []
+
+    for element in file_data.readlines():
+        raw_data.append(round(float(element), decimals))
+
+    return raw_data
+
+
+def runsTestPassed(file_name, decimals):
+    list = readList(file_name, decimals)
+    result_list = checkSign(list)
+    numberStreak = getStreak(result_list)
+    totalSign = countSignNumber(result_list)
+    expVal = getExpectedValue(totalSign)
+    variVal = getVarinece(totalSign)
+    return getRunTestResult(expVal, variVal, numberStreak)
