60ed0a68102cb196f31f8e1bdc9b9f34853a2ddb
liaDiaz <46771468+liaDiaz@users.noreply.github.com>
2022-07-27 19:17:18 -0500

    proyecto 3

---
+++ proyecto-3/p3.py
@@ -0,0 +1,95 @@
+import numpy as np
+import random
+import pandas as pd
+
+def estocasticMatrix(n):
+    a = 1
+    b = 10
+    matrix = (b-a)*np.random.random_sample((n,n)) +a
+    #columnas
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                matrix[i][j] = 0.0
+    for x in range(n):
+        matrix[x] = matrix[x]/ np.sum(matrix[x])
+    for index, value in enumerate(matrix):
+        print(index +1, value)
+    print(" ")
+    return(matrix)
+
+def createEnemi(n):
+    print("Number of warriors for each group")
+    values = []
+    for i in range(n):
+        print("Enemies", (i+1), 30)
+        values.append(30)
+    enemis = dict(list(enumerate(values)))
+    #Print dicctionary key enemy and value warriors
+    #print(enemis)
+    print("============================ ")
+    print(" ")
+    return enemis
+
+def attack(n, matrix, col, matrixEnemis):
+    maximum = 0
+    row = matrix[col]
+    rowSort = np.sort(row, axis=0)
+    interval = []
+    intervals = []
+    dicKey = 0
+    arrayKeys = []
+    annihilatedTeam = 0
+    group = []
+    g = 0
+    for element in rowSort:
+        interval.append(element)
+    interval.append(1.0)
+    for r in range(n):
+        allInterval = pd.Interval(interval[r], interval[r+1])
+        intervals.append(allInterval.length)
+    #print(intervals)
+    maximum = max(intervals)
+    arrayValues  = matrixEnemis.values()
+    while all(x > 0 for x in arrayValues):
+        for index, value in enumerate(intervals):
+            if maximum == value:
+                dicKey = index
+                arrayKeys.append(dicKey)
+            
+            for elementKey in arrayKeys:
+                if col == elementKey and elementKey != 0:
+                    matrixEnemis[elementKey-1] = matrixEnemis[elementKey-1]-1
+                elif col == elementKey and elementKey== 0:
+                     matrixEnemis[elementKey+1] = matrixEnemis[elementKey+1]-1
+                elif col != elementKey:
+                    matrixEnemis[elementKey] = matrixEnemis[elementKey]-1
+                group.append(elementKey)
+            #For print group
+            for index, value in enumerate(group):
+                g = value+1
+            print("attack of enemies", col +1, "to Group" , g)
+            #For print enemis
+            for key in matrixEnemis:
+                print("Group", key+1, ':', matrixEnemis[key])
+            
+        for index, value in matrixEnemis.items():
+            if value <= 0:
+                annihilatedTeam = index+1
+                print("Group",annihilatedTeam,"is annihilated!")
+    print("============================ ")
+    return annihilatedTeam
+        
+
+        
+
+def main():
+    n = int(input("Enter the number of team = "))
+    matrix = estocasticMatrix(n)
+    matrixEnemis = createEnemi(n)
+    attack(n, matrix, random.randint(0, n-1), matrixEnemis)
+    
+    
+    
+if __name__=="__main__":
+    main()
